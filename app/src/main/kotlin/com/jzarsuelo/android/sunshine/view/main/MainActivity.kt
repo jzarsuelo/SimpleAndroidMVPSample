74316e81fff33a81e9565ca21c54e9c7e5419cb7
Pao <jzarsuelo.dev@gmail.com>
2018-04-27 14:44:08 +1000

    [US0002] - Added resources; Implemented Toolbar

--- app/src/main/kotlin/com/jzarsuelo/android/sunshine/view/main/MainActivity.kt
+++ app/src/main/kotlin/com/jzarsuelo/android/sunshine/view/main/MainActivity.kt
@@ -1,10 +1,10 @@
 package com.jzarsuelo.android.sunshine.view.main
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import com.jzarsuelo.android.sunshine.R
 import com.jzarsuelo.android.sunshine.app.Injection
-import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.toolbar_main.*
 
 class MainActivity : AppCompatActivity(), MainContract.View {
 
@@ -12,12 +12,13 @@ class MainActivity : AppCompatActivity(), MainContract.View {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        requestDataButton.setOnClickListener { presenter.onRequestData() }
+        setSupportActionBar(mainToolbar)
+        supportActionBar?.setDisplayShowHomeEnabled(true)
+        supportActionBar?.setLogo(R.mipmap.ic_launcher)
     }
 
     override var presenter: MainContract.Presenter = MainPresenter(this, Injection.forecastRepository())
 
     override fun showData(data: String) {
-        helloWorldTextView.text = data
     }
 }
